e4fcf20465454a4ad205fc71bf0689d4dfabd03d
arcoshin <arcoshin@gmail.com>
2023-04-30 21:48:05 +0800

    2303

---
+++ src/main/java/com/tedu/SpringBoot2303Application.java
@@ -0,0 +1,13 @@
+package com.tedu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot2303Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot2303Application.class, args);
+    }
+
+}
